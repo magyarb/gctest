366054279fe4b8f461bc7de8f8e5756c03aa6983
magyar.balazs <magyarb94@gmail.com>
2020-04-24 00:47:44 +0200

    task

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ const dbo = require("./db");
 
 router.get("asd", (ctx, next) => {
   var log = new dbo.models.Log({
-    text: 'Hello!'
+    text: "Hello!",
   });
 
   log.save();
@@ -14,5 +14,55 @@ router.get("asd", (ctx, next) => {
   ctx.body = "log saved";
 });
 
+router.get("task", async (ctx, next) => {
+  const { CloudTasksClient } = require("@google-cloud/tasks");
 
-module.exports = router
+  // Instantiates a client.
+  const client = new CloudTasksClient();
+
+  // TODO(developer): Uncomment these lines and replace with your values.
+  const project = "crtst-273418";
+  const queue = "my-queue";
+  const location = "europe-west4";
+  const url = "https://gctst-nzk3w4wvsa-ez.a.run.app/api/asd";
+  const serviceAccountEmail =
+    "229996663812-compute@developer.gserviceaccount.com";
+  //const payload = 'Hello, World!';
+
+  // Construct the fully qualified queue name.
+  const parent = client.queuePath(project, location, queue);
+
+  const task = {
+    httpRequest: {
+      httpMethod: "GET",
+      url,
+      oidcToken: {
+        serviceAccountEmail,
+      },
+    },
+  };
+
+  if (payload) {
+    task.httpRequest.body = Buffer.from(payload).toString("base64");
+  }
+
+  if (inSeconds) {
+    // The time when the task is scheduled to be attempted.
+    task.scheduleTime = {
+      seconds: inSeconds + Date.now() / 1000,
+    };
+  }
+
+  console.log("Sending task:");
+  console.log(task);
+  // Send create task request.
+  const request = { parent, task };
+  const [response] = await client.createTask(request);
+  const name = response.name;
+
+  console.log(`Created task ${name}`);
+
+  ctx.body = "task queued";
+});
+
+module.exports = router;
